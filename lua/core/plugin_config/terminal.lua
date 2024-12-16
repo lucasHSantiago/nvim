274f2cbc67d101e9eae12bf660188decf9b1fc32
lucashsantiago <lucashenriquesantiago@gmail.com>
2024-12-16 11:53:12 -0300

    Reconfig terminal

--- lua/core/plugin_config/terminal.lua
+++ lua/core/plugin_config/terminal.lua
@@ -1,15 +1,13 @@
-require("toggleterm").setup{
-    size = 20,
-    open_mapping = [[<c-\>]],
-    hide_numbers = true,
-    shade_filetypes = {},
-    shade_terminals = true,
-    shading_factor = 2,
-    start_in_insert = true,
-    insert_mappings = true,
-    persist_size = true,
-    direction = "tab",
-    close_on_exit = true,
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-}
+require("toggleterm").setup({
+	size = 12,
+	open_mapping = [[<c-\>]],
+	hide_numbers = true,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "horizontal",
+	close_on_exit = false,
+	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+})
 
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])

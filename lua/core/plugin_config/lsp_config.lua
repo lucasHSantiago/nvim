ddaadf0f30ae796bd92b99ce9f0cbf0664b4dae6
lucashsantiago <lucashenriquesantiago@gmail.com>
2024-12-16 17:20:43 -0300

    Alterated for use omnisharp

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -11,12 +11,27 @@ require("mason-lspconfig").setup({
 })
 
 local lspconfig = require("lspconfig")
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
+})
+
 lspconfig.lua_ls.setup({})
 lspconfig.clangd.setup({})
-lspconfig.csharp_ls.setup({})
+-- lspconfig.csharp_ls.setup({})
 lspconfig.gopls.setup({})
-lspconfig.omnisharp.setup({})
 lspconfig.yamlls.setup({})
+
+lspconfig.omnisharp.setup({
+	capabilities = capabilities,
+	enable_roslyn_analysers = true,
+	enable_import_completion = true,
+	organize_imports_on_format = true,
+	enable_decompilation_support = true,
+	filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
+})
 
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- lua/core/plugin_config/none-ls.lua
+++ lua/core/plugin_config/none-ls.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.csharpier,
+        null_ls.builtins.formatting.omnisharp,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports,
 	},

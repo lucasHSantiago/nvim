2aabe2eeecd850af79701dc78136b401aa9614ef
lucashsantiago <lucashenriquesantiago@gmail.com>
2024-12-16 11:08:15 -0300

    Removed bufferline

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -53,12 +53,4 @@ require("lazy").setup({
 	},
 
 	"jiangmiao/auto-pairs",
-
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-	},
 })

f959ac389d17f94c04f034cbfd1c13d1881c423f
lucashsantiago <lucashenriquesantiago@gmail.com>
2024-12-18 09:46:49 -0300

    Added telescope buffer keymap

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -4,6 +4,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<c-p>", builtin.find_files, {})
 vim.keymap.set("n", "<Space><Space>", builtin.oldfiles, {})
 vim.keymap.set("n", "<Space>fg", builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
 
 require("telescope").load_extension("ui-select")

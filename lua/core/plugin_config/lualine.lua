e0ad3d42a04a25605b9f2ab6f95003bb4a5657c3
lucashsantiago <lucashenriquesantiago@gmail.com>
2025-01-20 19:17:12 -0300

    Change lualine visual

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -2,10 +2,12 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
+		section_separators = "",
+		component_separators = "",
 	},
 	sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diagnostics" },
 		lualine_c = {
 			{
 				"filename",

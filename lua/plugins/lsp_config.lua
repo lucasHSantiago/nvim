3cbedd5adbff7e7938c1137b3357a060b77d9016
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-02-06 16:33:04 -0300

    Added diagnostics float window

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,3 +1,62 @@
+local function keymaps()
+    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename, {})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, {})
+
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, {})
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {})
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {})
+end
+
+local function setup_lsp()
+    local lspconfig = require("lspconfig")
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+    })
+
+    lspconfig.lua_ls.setup({})
+
+    lspconfig.clangd.setup({})
+
+    lspconfig.gopls.setup({})
+
+    lspconfig.yamlls.setup({})
+
+    lspconfig.dockerls.setup({})
+
+    lspconfig.omnisharp.setup({})
+
+    lspconfig.rust_analyzer.setup({})
+
+    lspconfig.html.setup({})
+
+    lspconfig.cssls.setup({})
+
+    lspconfig.elixirls.setup({})
+
+    -- lspconfig.lexical.setup({})
+end
+
+local function setup_diagnostics()
+    vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    })
+end
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -5,6 +64,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         "OmniSharp/omnisharp-vim",
         "kevinhwang91/nvim-hlslens",
+        "j-hui/fidget.nvim",
     },
     config = function()
         vim.opt.signcolumn = "yes"
@@ -13,6 +73,7 @@ return {
         lspconfig_defaults.capabilities =
             vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+        require("fidget").setup({})
         require("mason").setup()
 
         require("mason-lspconfig").setup({
@@ -20,42 +81,8 @@ return {
             -- ensure_installed = { "lua_ls", "clangd", "omnisharp", "gopls", "dockerls", "yamlls", "rust_analyzer" },
         })
 
-        local lspconfig = require("lspconfig")
-
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-        })
-
-        lspconfig.lua_ls.setup({})
-
-        lspconfig.clangd.setup({})
-
-        lspconfig.gopls.setup({})
-
-        lspconfig.yamlls.setup({})
-
-        lspconfig.dockerls.setup({})
-
-        lspconfig.omnisharp.setup({})
-
-        lspconfig.rust_analyzer.setup({})
-
-        lspconfig.html.setup({})
-
-        lspconfig.cssls.setup({})
-
-        lspconfig.elixirls.setup({})
-
-        -- lspconfig.lexical.setup({})
-
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename, {})
-        vim.keymap.set("n", "<leader>ro", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, {})
+        setup_lsp()
+        setup_diagnostics()
+        keymaps()
     end
 }

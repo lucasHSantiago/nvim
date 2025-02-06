a14e76ed935a15041df0ef4a09a8338331e04872
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-02-06 11:08:01 -0300

    Added autopairs

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,17 +1,8 @@
 return {
-    -- {
-    --     "loctvl842/monokai-pro.nvim",
-    --     config = function()
-    --         require("monokai-pro").setup({
-    --             filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
-    --         })
-    --         vim.cmd([[colorscheme monokai-pro]])
-    --     end
-    -- },
     {
         "Mofiqul/dracula.nvim",
         config = function()
             vim.cmd [[colorscheme dracula]]
         end
-    },
+    }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require 'nvim-treesitter.configs'.setup {
-            ensure_installed = { "lua" },
+            ensure_installed = { "lua", "heex", "elixir", "erlang", "html" },
 
             sync_install = false,
             auto_install = true,

a682c5523d927b3474632886dffb2ba5d2cdd9f3
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-02-07 11:20:08 -0300

    removing terminal

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -2,6 +2,11 @@ return {
     {
         "Mofiqul/dracula.nvim",
         config = function()
+            require("dracula").setup({
+                colors = {
+                    bg = "#1E202C"
+                }
+            })
             vim.cmd [[colorscheme dracula]]
         end
     }

c6e3e6a88278340287e494abb8856ea2ed96dd9a
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-03-27 18:39:37 -0300

    Added snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -88,14 +88,7 @@ return {
 				severity_sort = true,
 				float = { border = "rounded", source = "if_many" },
 				underline = { severity = vim.diagnostic.severity.ERROR },
-				signs = vim.g.have_nerd_font and {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "󰅚 ",
-						[vim.diagnostic.severity.WARN] = "󰀪 ",
-						[vim.diagnostic.severity.INFO] = "󰋽 ",
-						[vim.diagnostic.severity.HINT] = "󰌶 ",
-					},
-				} or {},
+				signs = {},
 				virtual_text = {
 					source = "if_many",
 					spacing = 2,
@@ -230,12 +223,13 @@ return {
 					-- `friendly-snippets` contains a variety of premade snippets.
 					--    See the README about individual language/framework/plugin snippets:
 					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
+							require("luasnip.loaders.from_vscode").load({ paths = "./snippets" })
+						end,
+					},
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",

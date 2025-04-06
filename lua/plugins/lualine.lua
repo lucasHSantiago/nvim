f4301eaa1724fbf372e83d7b24d048ad1eb239f1
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-04-06 17:26:43 -0300

    Added lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				section_separators = "",
+				component_separators = "",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {},
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = {},
+				lualine_y = { "branch", "diagnostics" },
+				lualine_z = {},
+			},
+		})
+	end,
+}
